e062425415dc831062d19a47bfbaf550ccf81ae9
Mirkamand Seyidli <mirkemend97@gmail.com>
2024-01-15 17:21:07 +0400

    Created sticky navbar, overlay searchbar and overlay menu

--- script.js
+++
@@ -1,5 +0,0 @@
-const hamburger_menu = document.querySelector(".hamburger_menu");
-
-hamburger_menu.addEventListener("click", () => {
-  hamburger_menu.classList.toggle("active");
-});
---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const main_container = document.querySelector(".main_container");
+const hamburger_menu = document.querySelector(".hamburger_menu");
+const menu_overlay = document.querySelector(".menu_overlay");
+const search_overlay = document.querySelector(".search_overlay");
+const search_btn = document.querySelector(".search_btn");
+const primary_nav_wrapper = document.querySelector(".primary_nav_wrapper");
+
+hamburger_menu.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("active");
+  menu_overlay.classList.toggle("show");
+  primary_nav_wrapper.classList.toggle("sticky_search");
+});
+
+window.addEventListener("scroll", function () {
+  primary_nav_wrapper.classList.toggle("sticky", window.scrollY > 70);
+});
+
+search_btn.addEventListener("click", () => {
+  main_container.classList.toggle("overflow_hidden");
+  search_overlay.classList.toggle("show");
+  primary_nav_wrapper.classList.toggle("sticky_search");
+  search_btn.classList.toggle("active");
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target.classList.value == "search_overlay show") {
+    search_overlay.classList.toggle("show");
+    search_btn.classList.toggle("active");
+    primary_nav_wrapper.classList.toggle("sticky_search");
+  }
+});
